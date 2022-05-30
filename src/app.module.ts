b49144e8bf3bb525617a4b9232efdeca0dea6815
es5es5 <es5e5s@nate.com>
2022-05-31 00:51:56 +0900

    TypeORM init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeORMConfig } from './configs/typeorm.config';
 import { MicroorganismModule } from './microorganism/microorganism.module';
 import { OriginModule } from './origin/origin.module';
 
 @Module({
-  imports: [MicroorganismModule, OriginModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), MicroorganismModule, OriginModule],
   controllers: [AppController],
   providers: [AppService],
 })
