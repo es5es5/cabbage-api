4df4a1acdac81414070081bdbe066184e545ca29
es5es5 <es5e5s@nate.com>
2022-06-05 16:43:44 +0900

    typeorm.config.ts

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,6 +1,5 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-
 ConfigModule.forRoot()
 
 export const typeORMConfig: TypeOrmModuleOptions = {
@@ -11,5 +10,5 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_DATABASE,
   entities: ['dist/**/*.entity.{ts,js}'], // Entity 연결
-  synchronize: false, //true 값을 설정하면 어플리케이션을 다시 실행할 때 엔티티안에서 수정된 컬럼의 길이 타입 변경값등을 해당 테이블을 Drop한 후 다시 생성해준다.
+  synchronize: true, //true 값을 설정하면 어플리케이션을 다시 실행할 때 엔티티안에서 수정된 컬럼의 길이 타입 변경값등을 해당 테이블을 Drop한 후 다시 생성해준다.
 }
