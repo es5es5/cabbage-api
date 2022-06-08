94d05dfdde4abd5e0bb794c8b327fcc0a526857e
es5es5 <es5e5s@nate.com>
2022-06-08 16:36:56 +0900

    Auth init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
 import { MicroorganismModule } from './microorganism/microorganism.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), MicroorganismModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), MicroorganismModule, AuthModule],
   controllers: [],
   providers: [],
 })
