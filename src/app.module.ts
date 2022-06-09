21891f12a1232406d454f050edbe65056d59b83d
es5es5 <es5e5s@nate.com>
2022-06-09 10:34:40 +0900

    Clean App

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { typeORMConfig } from './configs/typeorm.config';
 import { MicroorganismModule } from './microorganism/microorganism.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeORMConfig), MicroorganismModule, AuthModule, UsersModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
