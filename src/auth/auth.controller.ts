724e6225d81e01154a7877d28a9073af4c7d2762
es5es5 <es5e5s@nate.com>
2022-06-09 18:02:49 +0900

    Set Cookie init

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, Res, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
 import { UsersDto } from 'src/users/users.dto';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
@@ -10,8 +11,19 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  async login(@Body() users: UsersDto) {
-    return this.authService.login(users);
+  async login(@Body() users: UsersDto, @Request() req: Request, @Res() res: Response) {
+    const { accessToken } = await this.authService.login(users);
+
+    res
+      .set('Access-Control-Allow-Credentials', 'true')
+      .set('Access-Control-Allow-Origin', origin)
+      .cookie('accessToken', accessToken, {
+        expires: new Date(new Date().getTime() + 60 * 1000 * 60 * 2),
+        sameSite: 'none',
+        secure: true,
+        httpOnly: false,
+      })
+    // return this.authService.login(users);
   }
 
   @UseGuards(JwtAuthGuard)
