cf153f32e7730b73db0f5e903ea40300834947d6
es5es5 <es5e5s@nate.com>
2022-06-10 16:30:32 +0900

    API PREFIX

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const PREFIX = 'rest'
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix(PREFIX);
 
   const swaggerConfig = new DocumentBuilder()
+    .setBasePath(PREFIX)
     .setTitle('Cabbage API')
     .setDescription('')
     .setVersion('1.0')
@@ -13,7 +17,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('swagger', app, document);
-  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
