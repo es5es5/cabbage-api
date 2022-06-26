fa6c50e9b5fcad38a59a29251931ed53f1160be1
es5es5 <es5e5s@nate.com>
2022-06-26 20:20:29 +0900

    StudyTopic Auth

--- src/study_topic/study_topic.controller.ts
+++ src/study_topic/study_topic.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Delete, Get, Param, Post, Put, Req, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { StudyTopicDto } from './study_topic.dto';
 import { StudyTopicService } from './study_topic.service';
 
@@ -9,26 +10,37 @@ export class StudyTopicController {
   constructor(private readonly studyTopicService: StudyTopicService) {}
 
   @Post()
-  create(@Body() studyTopic: StudyTopicDto) {
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('Authorization')
+  create(@Req() reqeust, @Body() studyTopic: StudyTopicDto) {
+    studyTopic.writerId = reqeust.user.userId
     return this.studyTopicService.create(studyTopic);
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('Authorization')
   findAll() {
     return this.studyTopicService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('Authorization')
   findOne(@Param('id') id: string) {
     return this.studyTopicService.findOne(+id);
   }
 
   @Put(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('Authorization')
   update(@Param('id') id: string, @Body() studyTopic: StudyTopicDto) {
     return this.studyTopicService.update(+id, studyTopic);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('Authorization')
   remove(@Param('id') id: string) {
     return this.studyTopicService.remove(+id);
   }
