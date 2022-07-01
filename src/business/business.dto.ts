66908baa258c44da9deafdc5909b0d4b9a582c5f
es5es5 <es5e5s@nate.com>
2022-07-01 15:23:21 +0900

    Business Description

--- src/business/business.dto.ts
+++ src/business/business.dto.ts
@@ -3,63 +3,63 @@ import { ApiProperty } from "@nestjs/swagger"
 export class BusinessDto {
   writerId: number
 
-  @ApiProperty()
+  @ApiProperty({ description: '사업명' })
   businessTitle: string
 
-  @ApiProperty()
+  @ApiProperty({ description: '내부 담당자' })
   businessManager: string
 
-  @ApiProperty()
+  @ApiProperty({ description: '과제명' })
   projectTitle: string
 
-  @ApiProperty()
+  @ApiProperty({ description: '사업명' })
   projectManager: string
 
-  @ApiProperty()
+  @ApiProperty({ description: '과제 상태' })
   projectState: string
 
-  @ApiProperty()
+  @ApiProperty({ description: '과제 PDF' })
   projectPdf: string
 
-  @ApiProperty()
+  @ApiProperty({ description: '주관부처' })
   supervisor: string
 
-  @ApiProperty()
+  @ApiProperty({ description: '사업비' })
   money: string
 
-  @ApiProperty()
+  @ApiProperty({ description: '과제번호' })
   projectNo: string
 
-  @ApiProperty()
+  @ApiProperty({ description: '' })
   department: string
 
-  @ApiProperty()
+  @ApiProperty({ description: '전문기관' })
   specializedOrganization: string
 
-  @ApiProperty()
+  @ApiProperty({ description: '발표상태' })
   presentationState: string
 
-  @ApiProperty()
+  @ApiProperty({ description: '주관연구기관' })
   supervisorStudyDepartment: string
 
-  @ApiProperty()
+  @ApiProperty({ description: '주관연구책임자' })
   supervisorStudyManager: string
 
-  @ApiProperty()
+  @ApiProperty({ description: '공동연구기관' })
   jointStudyDepartment: string
 
-  @ApiProperty()
+  @ApiProperty({ description: '공동연구책임자' })
   jointStudyManager: string
 
-  @ApiProperty()
+  @ApiProperty({ description: '위탁연구기관' })
   rentStudyDepartment: string
 
-  @ApiProperty()
+  @ApiProperty({ description: '위탁연구책임자' })
   rentStudyManager: string
 
-  @ApiProperty()
+  @ApiProperty({ description: '시작일자' })
   startDate: Date
 
-  @ApiProperty()
+  @ApiProperty({ description: '끝일자' })
   endDate: Date
 }
