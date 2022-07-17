eb6fe55daabb8b566b333bb78d0335de3fd8ef74
es5es5 <es5e5s@nate.com>
2022-07-17 17:46:24 +0900

    Bank code

--- src/bank/bank.service.ts
+++ src/bank/bank.service.ts
@@ -13,8 +13,8 @@ export class BankService {
 
   async create(bank: BankDto) {
     const result = await this.bankRepository.save(bank)
-    // 22-식품-0001
-    result.code = `${new Date().getFullYear().toString().substring(2)}-${result.category}-${result.id.toString().padStart(4, '0')}`
+    // 식품-22-0001
+    result.code = `${result.category}-${new Date().getFullYear().toString().substring(2)}-${result.id.toString().padStart(4, '0')}`
     return this.update(result.id, result)
   }
 
